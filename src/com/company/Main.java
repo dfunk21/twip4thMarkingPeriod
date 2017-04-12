94b8841f2d0d6e8f0d88f6d6a86a497eed719629
df377 <df377@pvsd.org>
2017-04-12 14:29:24 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the free-response grade generator!!");
+        Scanner sb = new Scanner(System.in);
+        
+
+    }
+}
